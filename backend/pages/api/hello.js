39ec18f3d339ff18c289e2a23ca043eb16ddf789
eltacitos <hugo.locatelli1@gmail.com>
2022-07-16 18:15:57 +0200

    Removed all the front of next, update hello/ to enable cors. Whipped the frontend to add a button to call hello/ et a variable to display the answer

--- backend/pages/api/hello.js
+++ backend/pages/api/hello.js
@@ -1,5 +1,23 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import Cors from "cors";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+const cors = Cors({
+  methods: ["GET", "HEAD", "POST"],
+});
+
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
+export default async function handler(req, res) {
+  await runMiddleware(req, res, cors);
+
+  res.status(200).json({ message: "General Kenobi" });
 }
