99ae612f3760662167f223c5f515043775b8ba43
eltacitos <hugo.locatelli1@gmail.com>
2022-07-16 18:15:57 +0200

    Removed all the front of next, update hello/ to enable cors. Whipped the frontend to add a button to call hello/ et a variable to display the answer

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+interface GeneralResponse {
+  message: string;
+}
 
 @Component({
   selector: "app-root",
@@ -7,4 +12,16 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   title = "frontend";
+  response = "";
+
+  // eslint-disable-next-line no-useless-constructor,no-unused-vars,no-empty-function
+  constructor(private http: HttpClient) {}
+
+  helloThere() {
+    this.http
+      .get<GeneralResponse>("http://localhost:3000/api/hello")
+      .subscribe((data: GeneralResponse) => {
+        this.response = data.message;
+      });
+  }
 }
