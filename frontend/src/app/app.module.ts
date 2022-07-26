965f33922b092e51badbf584af08f73c831b729b
eltacitos <hugo.locatelli1@gmail.com>
2022-07-26 12:57:08 +0200

    Added primeng, installed bootstrap4-light-purple theme, used there button

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
+import { ButtonModule } from 'primeng/button';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 @NgModule({
   bootstrap: [AppComponent],
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, ButtonModule, HttpClientModule],
   providers: [],
 })
 export class AppModule {}
